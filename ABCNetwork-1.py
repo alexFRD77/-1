e6d9e070d62e003b31f0a8bba2f7422db607af27
alexFRD77 <60537448+alexFRD77@users.noreply.github.com>
2020-06-12 20:03:06 +0300

    termux ofc lol

--- ABCNetwork-1.py
+++ ABCNetwork-1.py
@@ -2,12 +2,12 @@ import markovify
 ABCtext = open('Biden2020.txt', "r")
 # Build the model.
 text_model = markovify.Text(ABCtext, state_size=1)
-# Print five randomly-generated sentences
-print("write the name of the song name")
-TT=""
 # Uncomment this if you want to name your song
+#print("write the name of the song name")
 #TT = input()
+TT=""
 ThePower = TT + "\n"
+# Print ten randomly-generated sentences
 for i in range(10):
     C=text_model.make_sentence(tries=100)
     ThePower=ThePower+C+" \n"
